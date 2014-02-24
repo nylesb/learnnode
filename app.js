b05edfb2a0ae3397899c6cf8be0940463f6c7946
Nyles Breecher <nbreecher@gmail.com>
2014-02-23 21:04:24 -0600

    Created a signup and login system using mysql (commit by Asif on behalf of Nyles)

--- app.js
+++ app.js
@@ -34,25 +34,59 @@ app.get('/', function(req, res) {
 	res.sendfile("./views/index.html");
 });
 
-app.get('/login', function(req, res) {
+app.get('/users/login', function(req, res) {
 	res.sendfile("./views/login.html");
+});
+
+app.get('/users/signup', function(req, res) {
+	res.sendfile("./views/signup.html");
 });
 
 var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'nyles',
-  password : 'secret'
+  password : 'secret',
+  database: 'nyles'
+});
+
+app.get('/users', function(req, res) {
+	var query = "SELECT `username` FROM `users`";
+	
+	db.query(query, function(err, rows){
+		var text = rows;
+		res.send(JSON.stringify(text));
+	});
+});
+
+app.post('/users/signup', function(req, res) {
+	var body = JSON.stringify(req.body);
+	var query = "INSERT INTO `users` (`username`, `password`) VALUES (";
+	query += '"' + req.body.username + '", "' + req.body.password + '");';
+	
+	db.query(query, function(err, rows) {
+		var text = err || rows;
+		res.send(text);
+	});
 });
 
 app.post('/users/login', function(req, res) {
-	var body = JSON.stringify(req.body);
-	var query = "SELECT *";
+	var query = 'SELECT `password` FROM `users` WHERE `username`="' + req.body.username + '"';
 	
 	db.query(query, function(err, rows) {
-		var text = err | rows;
-		res.send(text);
+		if(err)
+		{
+			res.send(err + query);
+		}
+		var pw = rows[0].password;
+		if(pw == req.body.password)
+		{
+			res.send("The password matches!");
+		}
+		else
+		{
+			res.send("Wrong pw: " + JSON.stringify(pw));
+		}
 	});
-	
 });
 
 http.createServer(app).listen(app.get('port'), function(){
